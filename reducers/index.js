5d6e9e57a7ef9159a82c737a4889b1570cd4f277
Shaghayegh Taghipour <shaghayegh.taghipour@gmail.com>
2018-06-18 18:11:01 -0700

    Add the redux store

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,7 @@
-import { RECEIVE_ENTRIES,ADD_ENTRY } from '../actions'
+import { RECEIVE_ENTRIES, ADD_ENTRY } from '../actions'
 
 export default function entries (state = {}, action) {
+  //  state object - key is the specific day and the value is the metric for that day
   switch(action.type) {
     case RECEIVE_ENTRIES :
       return {
