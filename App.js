c7d57193d1d3fd3c4a4b96b79de18de12864e09d
Shaghayegh Taghipour <shaghayegh.taghipour@gmail.com>
2018-06-20 16:39:07 -0700

    Add StatusBar

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+} from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { Constants } from 'expo'
+import { purple } from './utils/colors'
+
+//  custome statusbar
+function UdaciStatusBar({backgroundColor, ...props}) {
+    return(
+      <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+        <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+      </View>
+    )
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex:1}}>
-          <View style={{height: 20}} />
+          {/* <View style={{height: 20}} />
+           */}
+           <UdaciStatusBar backgroundColor={purple}
+           barStyle="light-content"
+           />
           {/* <AddEntry /> */}
           <History />
         </View>
